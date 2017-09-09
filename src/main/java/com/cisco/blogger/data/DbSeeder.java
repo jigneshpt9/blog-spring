0e42d4d7bad55075eeaba15ecadd48f6e0f9becf
prpriyad <prpriyad@cisco.com>
2017-09-09 13:39:58 +0530

    @Autowired commited

--- src/main/java/com/cisco/blogger/data/DbSeeder.java
+++ src/main/java/com/cisco/blogger/data/DbSeeder.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,7 @@ import com.cisco.blogger.api.User;
 public class DbSeeder implements CommandLineRunner {
 	private BlogRepository blogRepository;
 	
-	
+	@Autowired
     public DbSeeder(BlogRepository blogRepository) {
         this.blogRepository = blogRepository;
     }
